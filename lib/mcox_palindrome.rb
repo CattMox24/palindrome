379b30814ed7a496561828ca25bc357268aa60ac
Matt <coxmattheu@gmail.com>
2022-08-11 12:33:55 -0500

    ruby tutorials

--- lib/mcox_palindrome.rb
+++ lib/mcox_palindrome.rb
@@ -2,7 +2,37 @@
 
 require_relative "mcox_palindrome/version"
 
-module McoxPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  #Returns the letters in the string
+  def letters
+
+	self.chars.select {|c| c.match(/[a-z]/i) }.join
+#	the_letters = []
+#	letter_regex = /[a-z]/i
+#	self.chars.each do |character|
+#		if character.match(letter_regex)
+#			the_letters << character
+#		end
+#	end
+#	the_letters.join	
+  end 
+
+ private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end
 end
+
+#module McoxPalindrome
+#  class Error < StandardError; end
+  # Your code goes here...
+#end
